447f1dd2436f0568cad5e39bd2b483e079866dbd
Joshua Oliphant <josholiphantlmp@gmail.com>
2016-04-17 13:45:23 -0700

    Changed from contact list to recipe box

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,19 +4,19 @@ myApp.controller('AppCtrl', ['$scope', '$http',
         console.log("Hello world from controller");
 
         var refresh = function() {
-            $http.get('/contactlist').success(function(response){
+            $http.get('/ingredientlist').success(function(response){
                 console.log("I got the data I requested");
-                $scope.contactlist = response;
-                $scope.contact = "";
+                $scope.ingredientlist = response;
+                $scope.ingredient = "";
 
             });
         };
 
         refresh();
 
-        $scope.addContact = function() {
-            console.log($scope.contact);
-            $http.post('/contactlist', $scope.contact).success(function(response){
+        $scope.addingredient = function() {
+            console.log($scope.ingredient);
+            $http.post('/ingredientlist', $scope.ingredient).success(function(response){
                 console.log(response);
                 refresh();
             });
@@ -24,26 +24,26 @@ myApp.controller('AppCtrl', ['$scope', '$http',
 
         $scope.remove = function(id) {
             console.log(id);
-            $http.delete('/contactlist/' + id).success(function(response){
+            $http.delete('/ingredientlist/' + id).success(function(response){
                 refresh();
             });
         };
 
         $scope.edit = function(id) {
             console.log(id);
-            $http.get('/contactlist/' + id).success(function(response){
-                $scope.contact = response;
+            $http.get('/ingredientlist/' + id).success(function(response){
+                $scope.ingredient = response;
             })
         }
 
         $scope.update = function() {
-            console.log($scope.contact._id);
-            $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response){
+            console.log($scope.ingredient._id);
+            $http.put('/ingredientlist/' + $scope.ingredient._id, $scope.ingredient).success(function(response){
                 refresh();
             })
         }
 
         $scope.deselect = function() {
-            $scope.contact = "";
+            $scope.ingredient = "";
         }
     }]);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('contactlist', ['contactlist']);
+var db = mongojs('ingredientlist', ['ingredientlist']);
 var bodyParser = require('body-parser');
 
 
@@ -9,41 +9,41 @@ var bodyParser = require('body-parser');
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-app.get("/contactlist", function(req, res){
+app.get("/ingredientlist", function(req, res){
     console.log("I received a GET request")
-    db.contactlist.find(function (err, docs) {
+    db.ingredientlist.find(function (err, docs) {
         console.log(docs);
         res.json(docs);
     });
 });
 
-app.post("/contactlist", function(req, res){
+app.post("/ingredientlist", function(req, res){
     console.log(req.body);
-    db.contactlist.insert(req.body, function(err, doc){
+    db.ingredientlist.insert(req.body, function(err, doc){
         res.json(doc);
     })
 });
 
-app.delete("/contactlist/:id", function(req, res){
+app.delete("/ingredientlist/:id", function(req, res){
     var id = req.params.id;
     console.log(id);
-    db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+    db.ingredientlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
         res.json(doc);
     });
 });
 
-app.get("/contactlist/:id", function(req, res){
+app.get("/ingredientlist/:id", function(req, res){
     var id = req.params.id;
     console.log(id);
-    db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
+    db.ingredientlist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
         res.json(doc);
     });
 });
 
-app.put("/contactlist/:id", function(req, res){
+app.put("/ingredientlist/:id", function(req, res){
     var id = req.params.id;
     console.log(req.body.name);
-    db.contactlist.findAndModify({query: {_id: mongojs.ObjectId(id)},
+    db.ingredientlist.findAndModify({query: {_id: mongojs.ObjectId(id)},
         update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
         new: true}, function(err, doc){
             res.json(doc);
