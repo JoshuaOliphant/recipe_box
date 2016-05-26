3bdf863cab85f999c8831adaebc139ce5ae34362
jtrimachi <jtrimachi1@gmail.com>
2016-05-25 20:46:37 -0700

    HI

---
+++ public/controllers/controller.js
@@ -0,0 +1,277 @@
+var recipeApp = angular.module('recipeApp', [
+    'ngRoute',
+    'ngResource',
+]);
+
+
+//controller for viewing a recipe box
+recipeApp.controller('recipeBoxCtrl', ['$scope', '$rootScope', '$http',
+    function($scope, $rootScope, $http){
+		//ng-init, loads categories
+        $scope.loadBox = function() {
+            $http.get('/categories').success(function(response){
+                console.log("I got the data I requested");
+                $scope.categorylist = response;
+                console.log(response);
+            });
+        };
+        
+		//stores the category ID in rootScope and redirects recipe list view
+        $scope.getrecipes = function(id){
+			$rootScope.category = id;
+			console.log($rootScope.category);
+        }
+		
+		//redirects to the create recipe view
+}]);
+
+//controller for viewing recipes in a category
+recipeApp.controller('recipesCtrl', ['$scope', '$rootScope', '$http',
+    function($scope, $rootScope, $http){
+		//ng-init, loads recipes 
+        $scope.loadRecipes = function() {
+            $http.get('/categories/' + $rootScope.category.categoryID).success(function(response){
+                console.log("I got the data I requested");
+                $scope.recipelist = response;
+                console.log(response);
+            });
+        };
+        
+		//stores the recipe id in rootScope and redirects to the recipe details view
+        $scope.getdetails = function(id) {
+			$rootScope.recipeID = id;
+			console.log($rootScope.recipeID);
+        }
+		
+		//redirects to the create recipe view
+       
+}]);
+
+//controller for viewing a notecard
+recipeApp.controller('recipeDetailCtrl', ['$scope', '$rootScope', '$http',
+    function($scope, $rootScope, $http){
+        
+        var ingredientIDs;
+		//ng-init, loads recipe details, including ingredients 
+        $scope.loadNotecard = function() {
+			$scope.categoryName = $rootScope.category.categoryName;
+            $http.get("/recipeData/" + $rootScope.recipeID).success(function(response) {
+                console.log("I got the data I requested");
+                $scope.recipe = response;
+				console.log(ingredientIDs);
+                ingredientIDs = response.ingredientIDs;
+                console.log(ingredientIDs);
+				console.log(ingredientIDs.length);
+                $scope.ingredients = [];
+                for (var i = 0; i < ingredientIDs.length; i++)
+                {
+                    var id = ingredientIDs[i].ingredientID;
+                    console.log(id);
+                    $http.get("/ingredientlist/" + id).success(function(response) {
+                        console.log(response);
+                        $scope.ingredients.push(response);
+                    });
+                }
+            });
+			console.log($scope.ingredients);
+        };
+		
+		//deletes a recipe from the db and redirects to the recipe list 
+	    $scope.removeRecipe = function(id) {
+            console.log(id);
+            $http.delete('/recipe/' + id).success(function(response){
+				console.log("Deleted recipe " + response);
+				window.location = "./#/recipes";
+            });
+			
+        };
+
+		//stores recipeID in the root scope and redirects to the edit page
+        $scope.edit = function(id) {
+			$rootScope.recipeID = id;
+            window.location = "./#/edit";
+        };
+		
+}]);
+
+//controller for creating a notecard 
+recipeApp.controller('createNotecardCtrl', ['$scope', '$rootScope', '$http',
+    function($scope, $rootScope, $http){
+		
+		var ingredientIDs = [];
+		//ng-init, loads categories into dropdown menu 
+		$scope.loadCreate = function() {
+			$scope.newingredient = "";
+			console.log("categories: ");
+			$http.get('/categories').success(function(response){
+                $scope.categories = response;
+            });
+			$scope.ingredients=[];
+        };
+		
+		//Creates a recipe and adds the ingredient ids
+		$scope.createrecipe = function() {
+			console.log(ingredientIDs);
+			$scope.recipe.ingredientIDs = ingredientIDs;
+			console.log($scope.recipe);
+			$http.post("/createrecipe", $scope.recipe).success(function(response) {
+				console.log("Recipe initialized");
+				console.log(response);
+				$rootScope.recipeID = response;
+				console.log("Sending recipe id " + $rootScope.recipeID);
+				window.location = "/#/recipeDetails";
+			});
+		};
+
+		//adds ingredients to the DB and stores the ID in array ingredientIDs
+		//adds ingredients to ingredientListForDisplay to be displayed in scope
+        $scope.addingredient = function() {
+			var id;
+            $http.post('/ingredientlist/', $scope.newingredient).success(function(response){
+				id = response;
+				console.log(id);
+				$scope.newingredient.ingredientID = id;
+				ingredientIDs.push(id);
+				$scope.ingredients.push(response);
+            });
+			$scope.newingredient = "";
+        };
+
+		//removes new ingredient from both card and DB
+        $scope.remove = function(id) {
+            console.log(id);
+            $http.delete('/ingredientlist/' + id).success(function(response){
+				console.log("Deleted ingredient " + id);
+            });
+			var j;
+			for (var i = 0; i < ingredientIDs.length; i++)
+			{
+				if (ingredientIDs[i].ingredientID = id)
+				{
+					j = i;
+					break;
+				}
+			}
+			if(j != -1) {
+				$scope.ingredients.splice(j, 1);
+				ingredientIDs.splice(j, 1);
+			}
+        };
+		
+		//returns to homepage
+		$scope.returnToHome = function() {
+			window.location = "/#";
+		}
+}]);
+
+//controler for editing recipes 
+recipeApp.controller('editRecipeCtrl', ['$scope', '$rootScope', '$http',
+    function($scope, $rootScope, $http){
+        var ingredientIDs;
+		//ng-init, loads all recipe data similar to the recipe details view 
+        $scope.loadEdit = function() {
+            $http.get("/recipeData/" + $rootScope.recipeID).success(function(response) {
+                console.log("I got the data I requested");
+                $scope.recipe = response;
+                ingredientIDs = response.ingredientIDs;
+                console.log(ingredientIDs);
+				console.log(ingredientIDs.length);
+                $scope.ingredients = [];
+                for (var i = 0; i < ingredientIDs.length; i++)
+                {
+                    var id = ingredientIDs[i].ingredientID;
+                    console.log(id);
+                    $http.get("/ingredientlist/" + id).success(function(response) {
+                        console.log(response);
+                        $scope.ingredients.push(response);
+                    });
+                }
+				console.log("Initial load ");
+				console.log($scope.recipe);
+            });
+			
+			//loads categories into dropdown 
+			$http.get('/categories').success(function(response){
+                $scope.categories = response;
+            });
+			console.log($scope.ingredients);
+        };
+		
+        //removes ingredient from card only, not the DB 
+		//assumption that an existing recipe may be using ingredients from another recipe 
+		$scope.deleteingredient = function(id) {
+			console.log(id);
+			var j;
+			for (var i = 0; i < ingredientIDs.length; i++)
+			{
+				if (ingredientIDs[i].ingredientID = id)
+				{
+					j = i;
+					break;
+				}
+			}
+			if(j != -1) {
+				$scope.ingredients.splice(j, 1);
+				ingredientIDs.splice(j, 1);
+			}
+			console.log($scope.ingredients);
+			console.log(ingredientIDs);
+		};
+		
+		//adds a new ingredient to the DB and the recipe
+		//allows duplicates in DB
+		$scope.addingredient = function() {
+			var id;
+            $http.post('/ingredientlist/', $scope.newingredient).success(function(response){
+				id = response;
+				console.log(id);
+				$scope.newingredient.ingredientID = id;
+				ingredientIDs.push(id);
+				$scope.ingredients.push(response);
+            });
+			$scope.newingredient = "";
+        };
+		
+		//saves the updated recipe in the DB
+		$scope.updaterecipe = function() {
+			console.log("I am in update recipe");
+			console.log($scope.recipe);
+			$scope.recipe.ingredientIDs = ingredientIDs;
+			console.log("I have added the ingredients");
+			console.log($scope.recipe);
+			$http.post("/updaterecipe", $scope.recipe).success(function(response){
+				console.log(response);
+				$rootScope.recipeID = $scope.recipe.recipeID;
+			});
+		};
+}]);
+
+
+recipeApp.config(function($routeProvider){
+    $routeProvider
+    .when('/#',{
+        templateUrl: 'recipeBox.html',
+        controller: 'recipeBoxCtrl'
+    })
+    .when('/recipes',{
+        templateUrl: '/recipes.html',
+        controller: 'recipesCtrl'
+    })
+    .when('/recipeDetails',{
+        templateUrl: '/viewRecipeDetails.html',
+        controller: 'recipeDetailCtrl'
+    })
+    .when('/create',{
+        templateUrl: '/createNotecard.html',
+        controller: 'createNotecardCtrl'
+    })
+	.when('/edit',{
+		templateUrl: '/editRecipeDetails.html',
+		controller: 'editRecipeCtrl'
+	})
+    .otherwise({
+        redirectTo: '/#'
+    });
+});
+
+
---
+++ server.js
@@ -0,0 +1,206 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var ingredientIDGenerator = 100;
+var recipeIDGenerator = 100;
+
+//mongoose schemas defined 
+var Recipes;
+var Categories;
+var Ingredients;
+
+//connect to DB
+var mongoDBConnection = require('./db.recipe_box.config');
+console.log("DB connection at: " + mongoDBConnection.uri);
+mongoose.connect(mongoDBConnection.uri);
+
+//define and model schemas 
+mongoose.connection.on('open', function () {
+    var Schema = mongoose.Schema;
+    var CategorySchema = new Schema(
+        {
+            categoryName: String,
+            categoryID: Number
+        },
+        {collection: 'categories'} //collection similar to table in RDBMS
+    );
+    Categories = mongoose.model('Categories', CategorySchema);
+
+    var RecipeSchema = new Schema(
+        {
+            recipeID: Number,
+            recipeName: String,
+            categoryID: Number,
+            recipeInstructions: String,
+            ingredientIDs: [{ingredientID: Number}]
+        },
+        {collection: 'recipes'}
+    );
+    Recipes = mongoose.model('Recipes', RecipeSchema);
+
+    var IngredientSchema = new Schema(
+        {
+            ingredientID: Number,
+            quantity: Number,
+			units: String,
+            ingredient: String,
+            caloriecount: Number
+        },
+        {collection: 'ingredients'}
+    );
+    Ingredients = mongoose.model('Ingredients', IngredientSchema);
+    console.log('models have been created');
+});
+
+//Define functions with Mongoose queries that can then be called by the Express routes 
+function retrieveCategories(res, query) {
+    var query = Categories.find(query);
+    query.exec(function (err, categoryArray) {
+        res.json(categoryArray);
+    });
+}
+
+function retrieveRecipesInCategory(res, query) {
+    var query = Recipes.find(query);
+    query.exec(function (err, recipeArray) {
+        res.json(recipeArray);
+    });
+}
+
+function retrieveRecipeData(res, query) {
+    var query = Recipes.findOne(query);
+    query.exec(function (err, recipeData) {
+        res.json(recipeData);
+    });
+}
+
+function retrieveIngredientData(res, query) {
+    console.log("In retrieveIngredientData");
+    var query = Ingredients.findOne(query);
+    query.exec(function (err, ingredientData) {
+        res.json(ingredientData);
+    });
+}
+
+//static location of files
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+//REST API routes 
+
+//retrieve all categories
+app.get("/categories", function (req, res) {
+    console.log("Query for all categories");
+    retrieveCategories(res, {});
+});
+
+//retrieve a given ingredient 
+app.get("/ingredientlist/:ingredientID", function (req, res) {
+    var id = req.params.ingredientID;
+    console.log("Query for ingredient: " + id);
+    retrieveIngredientData(res, {ingredientID: id});
+});
+
+//retrieve all recipes in a given category 
+app.get("/categories/:categoryID", function (req, res) {
+    var id = req.params.categoryID;
+    console.log("Query for category id: " + id);
+    retrieveRecipesInCategory(res, {categoryID: id});
+});
+
+//retrieve all data for a given recipe
+app.get("/recipeData/:recipeID", function (req, res) {
+    var id = req.params.recipeID;
+    console.log("Query for recipe id: " + id);
+    retrieveRecipeData(res, {recipeID: id});
+});
+
+//create an ingredient in DB; currently allows duplicates 
+app.post("/ingredientlist", function (req, res) {
+    var jsonObj = req.body;
+    console.log(jsonObj);
+    jsonObj.ingredientID = ingredientIDGenerator;
+    ingredientIDGenerator++;
+    Ingredients.create(jsonObj, function (err) {
+        if (err) {
+            console.log('Ingredient creation failed');
+        }
+    });
+	console.log(jsonObj);
+    res.send(jsonObj);
+});
+
+//creates a new recipe 
+app.post("/createrecipe", function (req, res) {
+    console.log(req.body);
+    var jsonObj = req.body;
+    jsonObj.recipeID = recipeIDGenerator;
+    recipeIDGenerator++;
+    Recipes.create(jsonObj, function (err) {
+        if (err) {
+            console.log('Recipe initialization failed');
+			console.log(err);
+        }
+    });
+
+    res.send(jsonObj.recipeID.toString());
+    console.log('Created: ' + jsonObj.recipeID);
+});
+
+//remove ingredient from DB
+app.delete("/ingredientlist/:ingredientid", function(req, res) {
+	var id = req.params.ingredientid;
+	console.log("Removing ingredient: " + id);
+	Ingredients.remove({ingredientID: id}, function(err) {
+		if (err) 
+		{
+			console.log("Unable to remove ingredient");
+		}
+	});
+	res.send(id);
+});
+
+//remove recipe from DB                  
+app.delete("/recipe/:recipeID", function(req, res) {
+	var id = req.params.recipeID;
+	console.log("Removing recipe: " + id);
+	Recipes.remove({recipeID: id}, function(err) {
+		if (err) 
+		{
+			console.log("Unable to remove recipe");
+		}
+	});
+	res.send(id);
+});
+
+//update existing recipe
+app.post("/updaterecipe", function(req, res) {
+	var id = req.body.recipeID;
+	console.log("Updating recipe " + id);
+	var recipeName = req.body.recipeName;
+	var categoryID = req.body.categoryID;
+	var recipeInstructions = req.body.recipeInstructions;
+	var ingredientIDs = req.body.ingredientIDs;
+	var options = {new: false};
+	var update = {recipeName, categoryID, recipeInstructions, ingredientIDs};
+	console.log(update);
+	Recipes.findOneAndUpdate({recipeID: id}, update, options, function(err) {
+		if (err)
+		{
+			console.log("Unable to update");
+			console.log(err);
+		}
+	});
+	res.send(update);
+});
+
+var port;
+if(process.env.port)
+    port = process.env.port;
+else
+    port = 3000;
+app.listen(port);
+
+console.log("Server running on port 3000");
