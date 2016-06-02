0768051a36c329e5b605a8cc4fc5773d773c7eb7
Joshua Oliphant <josholiphantlmp@gmail.com>
2016-06-01 21:28:16 -0700

    first tests pass

--- tests/testserver.js
+++ tests/testserver.js
@@ -3,8 +3,6 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var async = require('async');
 
-var server = require('/Users/joshuaoliphant/Dropbox/javascript_workspace/box/recipe_box/server');
-
 var assert = chai.assert;
 var expect = chai.expect;
 var should = chai.should();
@@ -19,12 +17,12 @@ describe('Test home page result', function () {
     var response;
 
     before(function(done) {
-        chai.request(server)
-        // chai.request('http://myrecipebox2.azurewebsites.net/#/%23')
-            .get('/categories/')
+        // chai.request("http://myrecipebox2.azurewebsites.net")
+        chai.request("http://localhost:3000")
+            .get('/categories')
             .end(function (err, res) {
                 requestResult = res.body;
-                console.log(requestResult);
+                console.log("requestResult = " + requestResult);
                 response = res;
                 done();
             });
@@ -32,14 +30,15 @@ describe('Test home page result', function () {
 
     it('Should return an array object with three objects', function(done){
         expect(response).to.have.status(200);
-        expect(requestResult).to.be.an.object;
-        expect(requestResult).to.have.length.equal(1);
+        expect(requestResult).to.be.an.array;
+        expect(requestResult.length).to.have.length.equal(3);
         expect(response).to.have.headers;
         done();
     });
+
     it('The first entry in the array has known properties', function(done){
         expect(requestResult[0]).to.include.keys('categoryName');
-        expect(response).to.have.deep.property('body[0].listId', '1');
+        expect(response).to.have.deep.property('body[0].categoryID', 1);
         expect(response.body).to.not.be.a.string;
         done();
     });
@@ -48,11 +47,8 @@ describe('Test home page result', function () {
         expect(response.body).to.satisfy(
             function (body) {
                 for (var i = 0; i < body.length; i++) {
-                    expect(body[i]).to.have.property('name');
-                    expect(body[i]).to.have.property('description');
-                    expect(body[i]).to.have.property('listId');
-                    expect(body[i]).to.have.property('state').to.have.length(1);
-                    expect(body[i]).to.have.property('owner').that.is.a('string');
+                    expect(body[i]).to.have.property('categoryName');
+                    expect(body[i]).to.have.property('categoryID');
                 }
                 return true;
             });
